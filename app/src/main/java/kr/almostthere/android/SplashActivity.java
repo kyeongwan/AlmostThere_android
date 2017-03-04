8550b32b5116c7ef17c0bfdba9fc3fb6d664a1b0
kyeongwan <lk@lemonlab.co.kr>
2017-03-04 20:43:09 +0900

    DeepLinkActivity 를 SplashActivity 로 변경

--- app/src/main/java/kr/almostthere/android/SplashActivity.java
+++ app/src/main/java/kr/almostthere/android/SplashActivity.java
@@ -1,45 +1,51 @@
 package kr.almostthere.android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
-
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-/**
- * Created by lk on 2017. 3. 4..
- */
+public class SplashActivity extends AppCompatActivity {
 
-public class DeepLinkActivity extends AppCompatActivity {
+    Context mContext;
 
     @Override
     public void onCreate(Bundle savedBundle){
         super.onCreate(savedBundle);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+        mContext = this;
 
         Intent intent = getIntent();
         if (intent == null || intent.getData() == null) {
-            finish();
+            startMainActivity();
+        }else{
+            getDeepLinkData();
         }
+
+    }
+
+    private void getDeepLinkData() {
+        Intent intent = getIntent();
         Uri uri = intent.getData();
         String param = uri.getQueryParameter("join");
 
         if(param != null){              // Kakao link version
             Toast.makeText(getApplicationContext(), param, Toast.LENGTH_SHORT).show();
         }else {                         // General Deep Link version
-            openDeepLink(uri);
+//            openDeepLink(uri);
         }
     }
 
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        setIntent(intent);
-    }
-
-    private void openDeepLink(Uri deepLink) {
-        String path = deepLink.getPath();
-        Toast.makeText(getApplicationContext(), path, Toast.LENGTH_SHORT).show();
+    private void startMainActivity() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(mContext, MainActivity.class));
+            }
+        }, 2000);
     }
 }
